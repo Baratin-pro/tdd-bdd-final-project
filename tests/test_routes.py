92162637b821dc5fd523ff303498784029a66647
Baratin.pro <baratin.pro@gmail.com>
2024-11-02 06:50:03 -0400

    Update project

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -250,14 +250,14 @@ class TestProductRoutes(TestCase):
         products = self._create_products(10)
         available_products = [product for product in products if product.available is True]
         available_count = len(available_products)        
-        # test for available
+   
         response = self.client.get(
             BASE_URL, query_string="available=true"
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), available_count)
-        # check the data just to be sure
+       
         for product in data:
             self.assertEqual(product["available"], True)
 
